9848da5cbe986f93e0674ad3a3c4b4b9cb781e95
Arghya Biswas <arghya.biswas@gmail.com>
2024-10-07 23:25:48 +0000

    try 3

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 cd .\src\ui
 npm run build
-xcopy /e /i build\* C:\inetpub\sites\dev\wwwroot\* /Y
--- install.bat
+++ install.bat
@@ -1,4 +1,2 @@
 cd .\src\ui
 npm install
-npm run build
-xcopy /e /i build\* C:\inetpub\sites\dev\wwwroot\* /Y
