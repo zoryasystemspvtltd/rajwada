51a80eaf5302f0acc00b576ee5350c2906d445ee
Arghya Biswas <arghya.biswas@gmail.com>
2024-10-09 22:27:26 +0000

    gantt chart

---
+++ src/ui/src/pages/app/Plan.jsx
@@ -0,0 +1,177 @@
+import React from "react";
+import { Task, ViewMode, Gantt } from "gantt-task-react";
+import { getStartEndDateForProject, initTasks } from "../../store/helper"
+import "gantt-task-react/dist/index.css";
+
+
+  export const ViewSwitcher= ({
+    onViewModeChange,
+    onViewListChange,
+    isChecked,
+  }) => {
+    return (
+      <div className="ViewContainer">
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.Hour)}
+        >
+          Hour
+        </button>
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.QuarterDay)}
+        >
+          Quarter of Day
+        </button>
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.HalfDay)}
+        >
+          Half of Day
+        </button>
+        <button className="Button" onClick={() => onViewModeChange(ViewMode.Day)}>
+          Day
+        </button>
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.Week)}
+        >
+          Week
+        </button>
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.Month)}
+        >
+          Month
+        </button>
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.Year)}
+        >
+          Year
+        </button>
+        <button
+          className="Button"
+          onClick={() => onViewModeChange(ViewMode.QuarterYear)}
+        >
+          Quarter Year
+        </button>
+        <div className="Switch">
+          <label className="Switch_Toggle">
+            <input
+              type="checkbox"
+              defaultChecked={isChecked}
+              onClick={() => onViewListChange(!isChecked)}
+            />
+            <span className="Slider" />
+          </label>
+          Show Task List
+        </div>
+      </div>
+    );
+  };
+
+// Init
+const SamplePlan = () => {
+  const [view, setView] = React.useState(ViewMode.Day);
+  const [tasks, setTasks] = React.useState(initTasks());
+  const [isChecked, setIsChecked] = React.useState(true);
+  let columnWidth = 65;
+  if (view === ViewMode.Year) {
+    columnWidth = 350;
+  } else if (view === ViewMode.Month) {
+    columnWidth = 300;
+  } else if (view === ViewMode.Week) {
+    columnWidth = 250;
+  }
+
+  const handleTaskChange = (task) => {
+    console.log("On date change Id:" + task.id);
+    let newTasks = tasks.map(t => (t.id === task.id ? task : t));
+    if (task.project) {
+      const [start, end] = getStartEndDateForProject(newTasks, task.project);
+      const project = newTasks[newTasks.findIndex(t => t.id === task.project)];
+      if (
+        project.start.getTime() !== start.getTime() ||
+        project.end.getTime() !== end.getTime()
+      ) {
+        const changedProject = { ...project, start, end };
+        newTasks = newTasks.map(t =>
+          t.id === task.project ? changedProject : t
+        );
+      }
+    }
+    setTasks(newTasks);
+  };
+
+  const handleTaskDelete = (task) => {
+    const conf = window.confirm("Are you sure about " + task.name + " ?");
+    if (conf) {
+      setTasks(tasks.filter(t => t.id !== task.id));
+    }
+    return conf;
+  };
+
+  const handleProgressChange = async (task) => {
+    setTasks(tasks.map(t => (t.id === task.id ? task : t)));
+    console.log("On progress change Id:" + task.id);
+  };
+
+  const handleDblClick = (task) => {
+    alert("On Double Click event Id:" + task.id);
+  };
+
+  const handleClick = (task) => {
+    console.log("On Click event Id:" + task.id);
+  };
+
+  const handleSelect = (task, isSelected) => {
+    console.log(task.name + " has " + (isSelected ? "selected" : "unselected"));
+  };
+
+  const handleExpanderClick = (task) => {
+    setTasks(tasks.map(t => (t.id === task.id ? task : t)));
+    console.log("On expander click Id:" + task.id);
+  };
+
+  return (
+    <div className="Wrapper">
+      <ViewSwitcher
+        onViewModeChange={viewMode => setView(viewMode)}
+        onViewListChange={setIsChecked}
+        isChecked={isChecked}
+      />
+      {/* <h3>Gantt With Unlimited Height</h3> */}
+      {/* <Gantt
+        tasks={tasks}
+        viewMode={view}
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onClick={handleClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        columnWidth={columnWidth}
+      /> */}
+      {/* <h3>Gantt With Limited Height</h3> */}
+      <Gantt 
+        tasks={tasks}
+        viewMode={view}
+        onDateChange={handleTaskChange}
+        onDelete={handleTaskDelete}
+        onProgressChange={handleProgressChange}
+        onDoubleClick={handleDblClick}
+        onClick={handleClick}
+        onSelect={handleSelect}
+        onExpanderClick={handleExpanderClick}
+        listCellWidth={isChecked ? "155px" : ""}
+        ganttHeight={300}
+        columnWidth={columnWidth}
+      />
+    </div>
+  );
+};
+
+export default SamplePlan;
