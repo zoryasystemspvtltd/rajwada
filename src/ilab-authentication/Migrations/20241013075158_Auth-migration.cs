090e12b2b0ec7527069d0b91cae139fa80032ffb
Adrish1999 <adrishbose198@gmail.com>
2024-10-20 09:54:33 +0530

    Initial migrations applied

---
+++ src/ilab-authentication/Migrations/20241013075158_Auth-migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlabAuthentication.Migrations
+{
+    /// <inheritdoc />
+    public partial class Authmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
