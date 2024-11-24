6536acb87c388fb8a454ab740a4424d5f18adeb8
Adrish1999 <adrishbose198@gmail.com>
2024-11-24 13:36:31 +0530

    Dependency Backend Model updated

--- src/raj-api/Data/Models/Dependency.cs
+++ src/raj-api/Data/Models/Dependency.cs
@@ -5,6 +5,7 @@ namespace RajApi.Data.Models
     public class Dependency : LabModel
     {
         public string Code { get; set; }
+        public string Type { get; set; }
         public string Description { get; set; }
     }
 }
