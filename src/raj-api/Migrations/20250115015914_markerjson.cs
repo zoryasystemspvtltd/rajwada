3ceccde9187936f89acfa8e9cf2d338923e63077
zoryarajiv <rajiv.mandal@zorya.co.in>
2025-01-15 07:31:07 +0530

    MarkerJson field added in Plan module

---
+++ src/raj-api/Migrations/20250115015914_markerjson.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RajApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class markerjson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MarkerJson",
+                table: "Plans",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationLogs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7433));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationLogs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7440));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationLogs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7445));
+
+            migrationBuilder.UpdateData(
+                table: "AssetTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7516));
+
+            migrationBuilder.UpdateData(
+                table: "AssetTypes",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7521));
+
+            migrationBuilder.UpdateData(
+                table: "AssetTypes",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7525));
+
+            migrationBuilder.UpdateData(
+                table: "Companys",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(6347));
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7330));
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 1, 15, 1, 59, 12, 186, DateTimeKind.Utc).AddTicks(7337));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarkerJson",
+                table: "Plans");
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationLogs",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7856));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationLogs",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7860));
+
+            migrationBuilder.UpdateData(
+                table: "ApplicationLogs",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7864));
+
+            migrationBuilder.UpdateData(
+                table: "AssetTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7932));
+
+            migrationBuilder.UpdateData(
+                table: "AssetTypes",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7936));
+
+            migrationBuilder.UpdateData(
+                table: "AssetTypes",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7938));
+
+            migrationBuilder.UpdateData(
+                table: "Companys",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7306));
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7769));
+
+            migrationBuilder.UpdateData(
+                table: "Departments",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 1, 7, 9, 13, 45, 358, DateTimeKind.Utc).AddTicks(7775));
+        }
+    }
+}
